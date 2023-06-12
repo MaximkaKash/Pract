3b1f1a685a1672eccf428bd6ea18a5e59bea95db
Maksim <87428288+MaximkaKash@users.noreply.github.com>
2023-06-12 12:42:50 +0300

    Update settings.py

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -186,8 +186,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'maximkanashyts@gmail.com'
-EMAIL_HOST_PASSWORD = 'yycymqgjjcjryngw'
+
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
